be4743ab2a9dfe15542f148afdc2a3bf7882330a
Bartek Muszynski <220bartek@gmail.com>
2020-04-12 11:30:08 -0700

    Oops... Forgot to add file

---
+++ x10/ui/composition/PrintConfig.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace x10.ui.composition {
+  public class PrintConfig {
+    public bool AlwaysPrintPath = false;
+  }
+}
