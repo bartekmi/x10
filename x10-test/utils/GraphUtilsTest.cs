33cdffa32afc58673aa4ab481a71318ac7dc612a
Bartek Muszynski <220bartek@gmail.com>
2020-05-16 02:29:41 -0600

    Start of generic graph utils

---
+++ x10-test/utils/GraphUtilsTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace x10.utils {
+  public class GraphUtilsTest {
+
+    [Fact]
+    public void Sort() {
+      int[] sorted = GraphUtils.SortDirectAcyclicGraph(new Edge<int>[] {
+        new Edge<int>(1, 3),
+        new Edge<int>(2, 4),
+        new Edge<int>(5, 1),
+        new Edge<int>(2, 5),
+      }).ToArray();
+
+      Assert.Equal(new int[] { 2, 4, 5, 1, 3 }, sorted);
+    }
+
+    [Fact]
+    public void SortWithCycle() {
+      try {
+        GraphUtils.SortDirectAcyclicGraph(new Edge<int>[] {
+          new Edge<int>(4, 1),
+          new Edge<int>(1, 2),
+          new Edge<int>(2, 3),
+          new Edge<int>(3, 1),
+        });
+        Assert.True(false);
+      } catch (Exception e) {
+        Assert.Equal("Circular reference(s) among: 1, 2, 3", e.Message);
+      }
+    }
+
+    [Fact]
+    public void SortWithSelfCycle() {
+      try {
+        GraphUtils.SortDirectAcyclicGraph(new Edge<int>[] {
+          new Edge<int>(1, 1),
+        });
+        Assert.True(false);
+      } catch (Exception e) {
+        Assert.Equal("Circular reference(s) among: 1", e.Message);
+      }
+    }
+  }
+}
---
+++ x10/utils/GraphUtils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace x10.utils {
+  public class Edge<T> {
+    public T From;
+    public T To;
+
+    public Edge(T from, T to) {
+      From = from;
+      To = to;
+    }
+  }
+
+  public static class GraphUtils {
+    // Nodes are derived from the edges, so orphaned nodes will not be included
+    public static List<T> SortDirectAcyclicGraph<T>(IEnumerable<Edge<T>> edges) {
+      Dictionary<T, int> _incomingNodesCount = edges
+        .GroupBy(x => x.To)
+        .ToDictionary(x => x.Key, x => x.Count());
+
+      foreach (T from in edges.Select(x => x.From))
+        if (!_incomingNodesCount.ContainsKey(from))
+          _incomingNodesCount[from] = 0;
+
+      Dictionary<T, List<Edge<T>>> _outgoingEdges = edges
+        .GroupBy(x => x.From)
+        .ToDictionary(x => x.Key, x => x.ToList());
+
+      List<T> orderedNodes = new List<T>();
+
+      while (_incomingNodesCount.Count > 0) {
+        List<T> toRemove = new List<T>();
+        foreach (var item in _incomingNodesCount) {
+          if (item.Value == 0) {
+            T node = item.Key;
+            toRemove.Add(node);
+            orderedNodes.Add(node);
+          }
+        }
+
+        if (toRemove.Count == 0 && _incomingNodesCount.Count > 0)
+          throw new Exception("Circular reference(s) among: " + string.Join(", ", _incomingNodesCount.Keys));
+
+        foreach (T node in toRemove) {
+          if (_outgoingEdges.TryGetValue(node, out List<Edge<T>> outgoingEdges))
+            foreach (Edge<T> edge in outgoingEdges)
+              _incomingNodesCount[edge.To] -= 1;
+          _incomingNodesCount.Remove(node);
+        }
+      }
+
+      return orderedNodes;
+    }
+  }
+}
