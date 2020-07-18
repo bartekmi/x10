0eb8ba59364b162f2d5b34a0fc3e1ce629d17f88
Bartek Muszynski <220bartek@gmail.com>
2020-07-17 21:18:10 -0600

    Oops forgot to add files

---
+++ x10/ui/UiUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using x10.ui.composition;
+
+namespace x10.ui {
+  public static class UiUtils {
+    /// <summary>
+    /// Recursively list all instances including this one and all descendents
+    /// </summary>
+    public static IEnumerable<Instance> ListAllInstances(Instance instance) {
+      List<Instance> instances = new List<Instance>();
+      ListAllInstances(instances, instance);
+      return instances;
+    }
+
+    private static void ListAllInstances(List<Instance> instances, Instance instance) {
+      instances.Add(instance);
+
+      foreach (Instance child in instance.ChildInstances)
+        ListAllInstances(instances, child);
+    }
+  }
+}
