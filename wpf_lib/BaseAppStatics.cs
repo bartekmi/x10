616a1a8be934dab66706b0d0d1511e91cc9b1119
Bartek Muszynski <220bartek@gmail.com>
2020-08-29 13:09:38 -0600

    Oops forgot to add files

---
+++ wpf_lib/BaseAppStatics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_lib;
+using wpf_lib.storybook;
+
+namespace wpf_lib {
+  public abstract class BaseAppStatics {
+    public static BaseAppStatics BaseSingleton { get; protected set; }
+
+    public INavigation Navigation { get; set; }
+
+    protected BaseAppStatics() {
+      // Do nothing
+    }
+  }
+}
---
+++ wpf_lib/lib/url_parsing/Parameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_lib.lib {
+  public class Parameters {
+    private List<Tuple<string, string>> _parameters = new List<Tuple<string, string>>();
+
+    internal void Add(string name, string value) {
+      _parameters.Add(new Tuple<string, string>(name, value));
+    }
+
+    public string GetParameter(string name) {
+      Tuple<string, string> parameter = _parameters.SingleOrDefault(x => x.Item1 == name);
+      if (parameter == null)
+        throw new Exception(string.Format("Parameter {0} does not exist", name));
+      return parameter.Item2;
+    }
+
+    public int GetParameterAsInt(string name) {
+      return int.Parse(GetParameter(name));
+    }
+  }
+}
