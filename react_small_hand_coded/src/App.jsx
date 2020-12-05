d17402a7db037580d0a2a4faf2d11b6e7b815b38
Bartek Muszynski <bartek@flexport.com>
2020-12-05 05:44:06 -0700

    Add a very basic menu at the top

--- react_small_hand_coded/src/App.jsx
+++ react_small_hand_coded/src/App.jsx
@@ -4,6 +4,10 @@ import * as React from "react";
 import { graphql, QueryRenderer } from "react-relay";
 import { Route, Router } from "react-router-dom";
 
+import Link from "latitude/Link";
+import Group from "latitude/Group";
+import Text from "latitude/Text";
+
 import './App.css';
 import BuildingsPage from "./pages/BuildingsPage";
 import TenantsPage from "./pages/TenantsPage";
@@ -13,12 +17,21 @@ import history from './history';
 export default function App(): React.Node {
   return (
     <Router history={history}>
+
+      <Group>
+        <Link href="/buildings">Buildings</Link>
+        <Link href="/buildings/new">New Building</Link>
+        <Link href="/tenants">Tenants</Link>
+      </Group>
+
       <Route exact path="/" component={BuildingsPage} />
       <Route exact path="/buildings" component={BuildingsPage} />
       <Route path="/buildings/edit/:id" component={BuildingEditPage} />
-      <Route path="/buildings/new" component={BuildingEditPage} />
-      <Route path="/tenants" component={TenantsPage} />
+      <Route exact path="/buildings/new" component={BuildingEditPage} />
+      <Route exact path="/tenants" component={TenantsPage} />
     </Router>
+
+
   );
 }
 
