6fff4daba322ca96f2985d4d3cd1706bc890d490
Bartek Muszynski <bartek@flexport.com>
2020-12-08 00:23:08 -0700

    Apparently, the import of react-datepicker.css was unnecessary!?!

--- react_small_hand_coded/src/index.jsx
+++ react_small_hand_coded/src/index.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import "react-datepicker/dist/react-datepicker.css";
+
+// Various external CSS's that need to be specially imported
+// I am actually not sure if this is necessary - tried removing it and couldn't
+// get things to fail. Scared to remove it because I spent ~2 days on this!
+// import "react-datepicker/dist/react-datepicker.css";
 
 import App from './App';
 
