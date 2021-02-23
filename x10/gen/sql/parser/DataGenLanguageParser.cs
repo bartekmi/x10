add1dc6d4cc52ea324a4e9e75142ae3619fd63de
Bartek Muszynski <bartek@flexport.com>
2021-02-22 20:54:33 -0700

    Continue previous refactor

--- x10/gen/sql/parser/DataGenLanguageParser.cs
+++ x10/gen/sql/parser/DataGenLanguageParser.cs
@@ -49,59 +49,6 @@ namespace x10.gen.sql.parser {
       Close = close;
     }
   }
-
-
-
-  #endregion
-
-  #region Tokenizer
-  internal class Tokenizer {
-    private int _index = 0;
-    private readonly string _text;
-
-    // Derived
-    internal bool HasMore { get { return _index < _text.Length; } }
-
-    internal Tokenizer(string text) {
-      _text = text;
-    }
-
-    internal char Next() {
-      if (!HasMore)
-        throw new Exception("Unexpected end of string");
-      return _text[_index++];
-    }
-
-    internal char? Peek() {
-      if (HasMore)
-        return _text[_index];
-      return null;
-    }
-
-    internal void Expect(char c) {
-      EatWhitespace();
-
-      char next = Next();
-      if (next != c)
-        throw new Exception(string.Format("Expected {0} but got {1} at index {2}", c, next, _index - 1));
-    }
-
-    internal void EatWhitespace() {
-      while (Peek() == ' ' || Peek() == '\t')
-        Next();
-    }
-
-    #region Mark
-    private int _mark;
-    internal void SetMark() {
-      _mark = _index;
-    }
-
-    internal void ResetToMark() {
-      _index = _mark;
-    }
-    #endregion
-  }
   #endregion
 
   #region Parser
