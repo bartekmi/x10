67cef6a517665089eaf152dbc400486828ddaf01
Bartek Muszynski <bartek@flexport.com>
2021-02-27 12:34:22 -0700

    Narrow return type of SetCompanyEntityAsPrimary mutation

--- hot_chocolate_small/ClientPage/HandCodedMutations.cs
+++ hot_chocolate_small/ClientPage/HandCodedMutations.cs
@@ -9,11 +9,21 @@ using x10.hotchoc.ClientPage.Entities;
 using x10.hotchoc.ClientPage.Repositories;
 
 namespace x10.hotchoc.ClientPage {
+  public class IdAndIsPrimary {
+    public string Id {get;set;}
+    public bool IsPrimary {get;set;}
+
+    internal IdAndIsPrimary(CompanyEntity entity) {
+      Id = entity.Id;
+      IsPrimary = entity.IsPrimary;
+    }
+  }
+
   public partial class Mutations {
     /// <summary>
     /// Set a particular Company Entity as the "Primary" - and sets all others to false.
     /// </summary>
-    public IEnumerable<CompanyEntity> SetCompanyEntityAsPrimary(
+    public IEnumerable<IdAndIsPrimary> SetCompanyEntityAsPrimary(
       string companyEntityId,
       [Service] IRepository repository) {
 
@@ -26,7 +36,7 @@ namespace x10.hotchoc.ClientPage {
         foreach (CompanyEntity entity in company.Entities)
           entity.IsPrimary = entity.Id == companyEntityId;
 
-        return company.Entities;
+        return company.Entities.Select(x => new IdAndIsPrimary(x));
     }
   }
 }
