69ef60eacd854882995a60c904d7959d0af74814
Bartek Muszynski <bartek@flexport.com>
2021-10-25 11:11:28 -0600

    Fix flow errors by relaxing typing on hand-coded functions

--- react_small_generated/src/dps/hasAddressMatches.js
+++ react_small_generated/src/dps/hasAddressMatches.js
@@ -1,10 +1,11 @@
 // @flow
 
 type MatchInfo = {
-  +addressMatchScore: number,
+  +addressMatchScore: ?number,
 };
 
 // Return true if there are any MatchInfos which have a match on 'address'
-export default function hasAddressMatches(matches: $ReadOnlyArray<MatchInfo>): boolean {
+export default function hasAddressMatches(matches: ?$ReadOnlyArray<MatchInfo>): boolean {
+  if (matches == null) return false;
   return matches.some(x => x.addressMatchScore && x.addressMatchScore > 85);
 }
