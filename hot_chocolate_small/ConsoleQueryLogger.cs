9b290d271359613ccec7c55e834d0e5f175c35bc
Bartek Muszynski <bartek@Barteks-MacBook-Pro.local>
2023-10-17 14:09:12 -0700

    Upgrade to latest hot chocolate

--- hot_chocolate_small/ConsoleQueryLogger.cs
+++ hot_chocolate_small/ConsoleQueryLogger.cs
@@ -7,73 +7,20 @@ using HotChocolate.Execution.Instrumentation;
 using Microsoft.Extensions.Logging;
 
 namespace x10.hotchoc {
-  public class ConsoleQueryLogger : DiagnosticEventListener {
-    private static Stopwatch _queryTimer;
+  public class ConsoleQueryLogger : ExecutionDiagnosticEventListener {
     private readonly ILogger<ConsoleQueryLogger> _logger;
-    public ConsoleQueryLogger(ILogger<ConsoleQueryLogger> logger) {
-      _logger = logger;
+
+    public ConsoleQueryLogger(ILogger<ConsoleQueryLogger> logger)
+        => _logger = logger;
+
+    public override IDisposable ExecuteRequest(IRequestContext context) {
+      Console.WriteLine("Console ExecuteRequest: " + context);
+      return EmptyScope;
     }
 
-    public override IActivityScope ExecuteRequest(IRequestContext context) {
-      return new RequestScope(_logger, context);
-    }
-
-    private class RequestScope : IActivityScope {
-      private readonly IRequestContext _context;
-      private readonly ILogger<ConsoleQueryLogger> _logger;
-      public RequestScope
-          (ILogger<ConsoleQueryLogger> logger,
-               IRequestContext context) {
-        _logger = logger;
-        _context = context;
-        _queryTimer = new Stopwatch();
-        _queryTimer.Start();
-      }
-
-      public void Dispose() {
-        if (_context.Document != null) {
-          StringBuilder stringBuilder =
-              new StringBuilder(_context.Document.ToString(true));
-          stringBuilder.AppendLine();
-          if (_context.Variables != null) {
-            // var variablesConcrete =
-            //     _context.Variables.ToList();
-            // if (variablesConcrete.Count > 0)
-            // {
-            //     stringBuilder.
-            //         AppendFormat($"Variables {Environment.NewLine}");
-            //     try
-            //     {
-            //         foreach (var variableValue in _context.Variables)
-            //         {
-            //             string PadRightHelper
-            //                 (string existingString, int lengthToPadTo)
-            //             {
-            //                 if (string.IsNullOrEmpty(existingString))
-            //                     return "".PadRight(lengthToPadTo);
-            //                 if (existingString.Length > lengthToPadTo)
-            //                     return existingString.Substring(0, lengthToPadTo);
-            //                 return existingString + " ".PadRight(lengthToPadTo - existingString.Length);
-            //             }
-            //             stringBuilder.AppendFormat(
-            //                 $"  {PadRightHelper(variableValue.Name, 20)} :  {PadRightHelper(variableValue.Value.ToString(), 20)}: {variableValue.Type}");
-            //             stringBuilder.AppendFormat($"{Environment.NewLine}");
-            //         }
-            //     }
-            //     catch
-            //     {
-            //         // all input type records will land here.
-            //         stringBuilder.Append("  Formatting Variables Error. Continuing...");
-            //         stringBuilder.AppendFormat($"{Environment.NewLine}");
-            //     }
-            // }
-          }
-          _queryTimer.Stop();
-          stringBuilder.AppendFormat(
-              $"Ellapsed time for query is {_queryTimer.Elapsed.TotalMilliseconds:0.#} milliseconds.");
-          _logger.LogInformation(stringBuilder.ToString());
-        }
-      }
-    }
+    public override void RequestError(IRequestContext context, Exception exception) {
+      Console.WriteLine("Console RequestError: " + context);
+      _logger.LogError(exception, "A request error occurred!");
+    }  
   }
 }
